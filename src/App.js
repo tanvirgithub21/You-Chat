95acb2c024149a82a21310fa1cce2841239ea0e7
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-09-27 14:47:36 +0600

    added background svg

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SideBar from './Component/SideBar/SideBar';
 
 function App() {
   return (
-    <div className="App grid place-items-center h-screen w-screen bg-gray-200">
+    <div className="app grid place-items-center h-screen w-screen bg-gray-200">
       <div className='flex h-[95%] w-[97%] bg-white shadow-xl'>
         <SideBar />
         <ChatBox />
